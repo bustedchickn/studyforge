1a342ce6a031e2caae8ce8b70c29630840f6f721
alexanderdombroski <dom21013@byui.edu>
2024-10-19 13:16:08 -0600

    attempted to fix the chrome storage

--- scripts/button-handler.js
+++ scripts/button-handler.js
@@ -10,17 +10,17 @@
 function reset(event) {
     event.target.hidden = true;
     document.getElementById("page-notes").hidden = true;
-    document.getElementById("home-grid").hidden = false;
-    document.getElementById("return").hidden = false;
+    document.getElementById("home-grid").style.display = "flex";
+    document.getElementById("return").hidden = true;
 }
 
 function openSettings() {
-    
+
 }
 
 
 function openReminders() {
     document.getElementById("return").hidden = false;
     document.getElementById("page-notes").hidden = false;
-    document.getElementById("home-grid").hidden = true;
+    document.getElementById("home-grid").style.display = "none";
 }
