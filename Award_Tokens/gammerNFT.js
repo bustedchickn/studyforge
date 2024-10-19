fd91ac3c2a7e1310e42d030cf583e8aeb109f74a
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-10-19 03:33:26 -0600

    I have finished!

--- Award_Tokens/gammerNFT.js
+++ Award_Tokens/gammerNFT.js
@@ -1,94 +1,75 @@
 // this is for the amount of times the finished button has bin hit
-let counter = 0;
+let count = 0;
 let item = 0;
 
 // const button = document.querySelector('.outline');
 
-
-//This function will be called everytie the finished assinment button is hit
-document.addEventListener('DOMContentLoaded', () => {
+const onPageLoad = () => {
     const bigButton = document.getElementById('c_finish_assingment');
     bigButton.addEventListener('click', incrementCounter);
-});
+    document.removeEventListener('DOMContentLoaded', onPageLoad);
+}
+
+//This function will be called everytie the finished assinment button is hit
+document.addEventListener('DOMContentLoaded', onPageLoad);
+
 //This function will be called every time the button is hit
 
 function incrementCounter(){
-    // Increment the counter for asinment to be done.
-    counter++;
+    console.log(count);
+    // Increment the count for asinment to be done.
+    count += 1;
     //for every 5 asinments done then display a token.
-    if(counter % 5 == 0){
-        item++
+    if(count % 5 == 0){
+        document.getElementById("awardSound").play();
+        const pain = document.getElementById('c_Outline');
+
+        pain.style.animation ="slide-in-out 4s ease-in-out forwards"
         fetch('../Award_Tokens/awards.json')
         .then(response => response.json())
         .then(data => {
-
-            const maxLength = data.length
+            const maxLength = data.length;
             //If the max item has been reached then do nothing
-            if(item >= maxLength){
-                return ;
-            }
-            // if the last item has not been displayed show the next one in line.
-            else{
-                // set the next token item to be assigned
+                //set the token to assigned
                 data[item].assigned = true
+                console.log(data);
                 //display the item
                 callItem(item, data);
                 // wait 6 seconds then remove the item
-                setTimeout(() => {
-                    removeItem(item);
-                }, 6000);
-            
+                // setTimeout(() => {
+                //     removeItem(item);
+                // }, 6000);
             }
-        })
-}
+        )
+        item++
+    }
 }
 /*************************************************************************
  * THis function is meant to take the data from the json and display it
  *************************************************************************/
 function callItem(item, data){
     // const listEL = document.getElementById('outline');
-    const listEL = document.createElement('button');
-    listEL.classList.add('c_Outline');
+    const listEL = document.getElementById('c_Outline');
     //Check if the token can be displayed only displayed assigned tokens
     if(data[item].assigned == true){ 
-        // Create the container div for the outline
-        const outlineDiv = document.createElement('div');
-        outlineDiv.classList.add('c_Outline');
 
         // Create the div for the image and add the image inside
-        const imgHolderDiv = document.createElement('div');
-        imgHolderDiv.classList.add('c_img_holder');
+        const imgHolderDiv = document.getElementById('c_img');
+        imgHolderDiv.src = data[item].image; // Set image source from JSON
+        imgHolderDiv.alt = data[item].name; // Set alt text as the award name
 
-        const imgEl = document.createElement('img');
-        imgEl.classList.add('c_img_token');
-        imgEl.src = data[item].image; // Set image source from JSON
-        imgEl.alt = data[item].name; // Set alt text as the award name
-
-        imgHolderDiv.appendChild(imgEl);
 
         // Create the div for the title and add the title inside
-        const titleHolderDiv = document.createElement('div');
-        titleHolderDiv.classList.add('c_title_holder');
-
-        const titleEl = document.createElement('h2');
-        titleEl.classList.add('c_titles');
-        titleEl.textContent = data[item].name; // Set the title from JSON
-
-        titleHolderDiv.appendChild(titleEl);
-
-        // Append the image and title to the outline div
-        outlineDiv.appendChild(imgHolderDiv);
-        outlineDiv.appendChild(titleHolderDiv);
-
-        // Finally, append the outline div to the main list element
-        listEL.appendChild(outlineDiv);
+        const titleHolderDiv = document.getElementById('c_title');
+        titleHolderDiv.innerText = data[item].name; // Set the title from JSON
     }
 }
 
-function removeItem(item) {
-    const tokenElement = document.querySelector('.c_Outline');
-    if (tokenElement) {
-        tokenElement.remove();  // Remove the element from the DOM
-    }
-}
+// function removeItem(item) {
+//     const tokenElement = document.querySelector('.c_Outline');
+//     if (tokenElement) {
+//         tokenElement.remove();  // Remove the element from the DOM
+//         console.log('Token removed');
+//     }
+// }
 
