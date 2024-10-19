091e28baa7f6c6684d8fa335f0b1805603999d36
alexanderdombroski <dom21013@byui.edu>
2024-10-19 14:10:25 -0600

    linked up pomo timer

--- extension/scripts/button-handler.js
+++ extension/scripts/button-handler.js
@@ -4,6 +4,7 @@
     document.getElementById("settings").addEventListener('click', openSettings);
     document.getElementById("reminders").addEventListener('click', openReminders);
     document.getElementById("return").addEventListener('click', reset);
+    document.getElementById("pomo-button").addEventListener('click', openPomodoro);
 
 })();
 
@@ -18,9 +19,12 @@ function openSettings() {
 
 }
 
-
 function openReminders() {
     document.getElementById("return").hidden = false;
     document.getElementById("page-notes").hidden = false;
     document.getElementById("home-grid").style.display = "none";
+}
+
+function openPomodoro() {
+    window.open('../pomodoro/pomodoro.html', 'Pomodoro Timer', 'width=200,height=250');
 }
